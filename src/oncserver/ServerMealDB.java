77643aebd30a186156518fa1331896fa7e9d5197
oneilljw <johnwoneill1@gmail.com>
2015-06-08 14:09:30 -0400

    added MealDB

---
+++ src/oncserver/ServerMealDB.java
@@ -0,0 +1,29 @@
+package oncserver;
+
+public class ServerMealDB extends ONCServerDB {
+
+	@Override
+	String add(int year, String userjson) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	void createNewYear(int year) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void addObject(int year, String[] nextLine) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void save(int year) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
