4d390371b81bf31c00d14fc1c5f190f51be2e2b2
oneilljw <johnwoneill1@gmail.com>
2015-09-12 19:39:39 -0400

    update for desktop client v2.51

--- src/oncserver/DesktopClient.java
+++ src/oncserver/DesktopClient.java
@@ -30,7 +30,7 @@ public class DesktopClient extends Thread
 {
 	private static final int BASE_YEAR = 2012;
 	private static final int NUMBER_OF_WISHES_PER_CHILD = 3;
-	private static final float MINIMUM_CLIENT_VERSION = 2.50f;
+	private static final float MINIMUM_CLIENT_VERSION = 2.51f;
 	
 	private int id;
 	private String version;
