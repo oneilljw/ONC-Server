8514bbaff9c1dcc8f0cbbe66fa50833f7a086472
oneilljw <johnwoneill1@gmail.com>
2015-09-25 15:58:15 -0400

    relase of v2.00 of server with desktop client v3.00

--- src/oncserver/ONCServer.java
+++ src/oncserver/ONCServer.java
@@ -23,7 +23,7 @@ public class ONCServer
 	 */
 	
 	private static final String APPNAME = "Our Neighbor's Child Server";
-	private static final String ONC_SERVER_VERSION = "Our Neighbor's Child Server Version 1.27\n";
+	private static final String ONC_SERVER_VERSION = "Our Neighbor's Child Server Version 2.00\n";
 	private static final String ONC_COPYRIGHT = "\u00A92015 John W. O'Neill";
 	private ServerUI serverUI;	//User IF
 	private ServerLoop serverIF; 	//Server loop
